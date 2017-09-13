46cc88dd6fde663e9893682ec4dcad7216fe80cd
WALINNS INNOVATION <walinnsinnovation@WALINNSs-Mac-mini.local>
2017-09-13 10:13:28 +0530

    Initial Commit

---
+++ WalinnsTracker/WalinnsTracker.h
@@ -0,0 +1,19 @@
+//
+//  WalinnsTracker.h
+//  WalinnsTracker
+//
+//  Created by WALINNS INNOVATION on 13/09/17.
+//  Copyright © 2017 WALINNS INNOVATION. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WalinnsTracker.
+FOUNDATION_EXPORT double WalinnsTrackerVersionNumber;
+
+//! Project version string for WalinnsTracker.
+FOUNDATION_EXPORT const unsigned char WalinnsTrackerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WalinnsTracker/PublicHeader.h>
+
+
