348b32516400f98588b34fc0e057e5114e8eeb61
Rejoyline <31914628+Rejoyline@users.noreply.github.com>
2017-09-14 09:25:06 +0530

    ios library

---
+++ WalinnsTracker/WalinnsLib.swift
@@ -0,0 +1,37 @@
+//
+//  WalinnsLib.swift
+//  WalinnsTracker
+//
+//  Created by WALINNS INNOVATION on 13/09/17.
+//  Copyright © 2017 WALINNS INNOVATION. All rights reserved.
+//
+
+import Foundation
+
+
+public class WalinnsLib
+{
+    
+private var isDebug: Bool!
+
+//2.
+public init() {
+    self.isDebug = false
+}
+
+//3.
+public func setup(isDebug: Bool) {
+    self.isDebug = isDebug
+    print("Project is in Debug mode: \(isDebug)")
+}
+
+//4.
+public func Print<T>(value: T) {
+    if self.isDebug == true {
+        print(value)
+    } else {
+        //Do any stuff for production mode
+    }
+}
+    
+}
